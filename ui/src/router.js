f7c5db1ebca570374e9d3879dac9cc18ca936f28
theoremoon <theoldmoon0602@gmail.com>
2019-10-29 15:51:39 +0900

    throw away

--- ui/src/router.js
+++ ui/src/router.js
@@ -2,8 +2,6 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 import Index from "./Index.vue";
-import Register from "./Register.vue";
-import Login from "./Login.vue";
 
 Vue.use(VueRouter);
 
@@ -12,14 +10,6 @@ export default new VueRouter({
     {
       path: "/",
       component: Index
-    },
-    {
-      path: "/register",
-      component: Register
-    },
-    {
-      path: "/login",
-      component: Login
     }
   ]
 });
