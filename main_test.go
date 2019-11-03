022af9a0c371503b3c049b8b37ef581e264f24f5
jiro4989 <jiroron666@gmail.com>
2019-11-03 20:46:57 +0900

    :white_check_mark: travarseDirectory関数のテストコードを追加

--- main_test.go
+++ main_test.go
@@ -60,3 +60,37 @@ func TestIsFile(t *testing.T) {
 		})
 	}
 }
+
+func TestTravarseDirectory(t *testing.T) {
+	const root = "testdata/travarse"
+	got, err := travarseDirectory(root)
+	assert.Nil(t, err)
+
+	assert.Equal(t, root, got.Name)
+	assert.Equal(t, root, got.Path)
+	assert.Equal(t, 2, len(got.Children))
+	assert.Equal(t, "001.txt", got.Children[0].Name)
+	assert.Equal(t, "/001.txt", got.Children[0].Path)
+	assert.Equal(t, "002.txt", got.Children[1].Name)
+	assert.Equal(t, "/002.txt", got.Children[1].Path)
+
+	assert.Equal(t, 1, len(got.ChildDirs))
+	assert.Equal(t, "003", got.ChildDirs[0].Name)
+	assert.Equal(t, "/003", got.ChildDirs[0].Path)
+
+	assert.Equal(t, 1, len(got.ChildDirs[0].Children))
+	assert.Equal(t, "001.txt", got.ChildDirs[0].Children[0].Name)
+	assert.Equal(t, "/003/001.txt", got.ChildDirs[0].Children[0].Path)
+}
+
+func TestVerifyPath(t *testing.T) {
+	// TODO
+}
+
+func TestVerifyID(t *testing.T) {
+	// TODO
+}
+
+func TestCompile(t *testing.T) {
+
+}
